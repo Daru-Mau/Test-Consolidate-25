22c73672d1469089ce04d18190ee17f3834f873a
Daniel <53621755+Daru-Mau@users.noreply.github.com>
2024-02-04 19:42:20 +0100

    Update main.py

--- my_python_project/main.py
+++ my_python_project/main.py
@@ -1,9 +1,9 @@
 # my_python_project/main.py
 
 def add_numbers(a, b):
-    # TODO: Implement this function
+    c = a+b
     pass
 
 if __name__ == "__main__":
-    # TODO: Add code for testing or demonstration
+    print(add_numbers(1,2))
     pass
